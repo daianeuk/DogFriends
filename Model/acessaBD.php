f6e9a144bbdb1f40933a19081994b297dcf3a614
DieUK <68020563+daianeuk@users.noreply.github.com>
2021-05-14 15:53:12 -0300

    Add files via upload

--- Model/acessaBD.php
+++ Model/acessaBD.php
@@ -62,4 +62,63 @@ try{
         }
         return $dadosPerfil;
     }
+    function atualizaPerfil($nomeAntigo,$senhaAntiga,$nome,$senha,$raca,$telefone,$dataDeNascimento,
+        $descricao, $foto, $sexo){
+            
+            global $conexao;
+            
+            $stmt = $conexao->prepare("UPDATE cachorros SET
+        nome = :nome, senha = :senha, raca = :raca, telefone =:telefone, dataDeNascimento =:dataDeNascimento,
+        descricao =:descricao, foto=:foto, sexo=:sexo
+        WHERE nome=:nomeAntigo and senha=:senhaAntiga");
+            
+            $stmt->bindParam(':nome', $nome);
+            $stmt->bindParam(':senha', $senha);
+            $stmt->bindParam(':raca', $raca);
+            $stmt->bindParam(':telefone', $telefone);
+            $stmt->bindParam(':dataDeNascimento', $dataDeNascimento);
+            $stmt->bindParam(':descricao', $descricao);
+            $stmt->bindParam(':foto', $foto);
+            $stmt->bindParam(':sexo', $sexo);
+            $stmt->bindParam(':nomeAntigo', $nomeAntigo);
+            $stmt->bindParam(':senhaAntiga', $senhaAntiga);
+            
+            $stmt->execute();
+            
+            echo "AtualizaÃ§Ã£o realizada com sucesso!";
+            return true;
+    }
+    
+    function exibeTodosOsCaes($nome, $senha){
+    global $conexao;
+    $rs = $conexao->query("SELECT * FROM cachorros WHERE nome!='$nome' OR senha!='$senha'");
+    while($row = $rs->fetch(PDO::FETCH_OBJ)){
+        echo "<form action = '../Controler/salvacoracoes.php' method='post'>";
+        list ($ano,$mes,$dia) = explode('-', $row->dataDeNascimento);
+        echo "<h4> $row->nome - $row->raca - $row->sexo <br> Data de Nascimento: $dia / $mes / $ano </h4>";
+        echo "<img src='imagens/$row->foto' width='20%'/> $row-> descricao <br><br>";
+        echo "<input type=hidden name=nome2 value='$row->nome'>";
+        echo "<input type=hidden name=senha2 value='$row->senha'>";
+        echo "<input type='submit' class='btn-danger' value='Enviar pedido(s) de amizade'></br><br>";
+        echo "</form>";
+    }
+    }
+    function salvaAmizade($nome,$senha,$nome2,$senha2){
+        global $conexao;
+        $stm = $conexao->prepare("INSERT INTO amizades(nome,senha,nome2,senha2) 
+                VALUES (:nome, :senha, :nome2, :senha2)");
+        $stm->bindParam(':nome', $nome);
+        $stm->bindParam(':senha', $senha);
+        $stm->bindParam(':nome2', $nome2);
+        $stm->bindParam(':senha2', $senha2);
+        
+        $stm->execute();
+        
+        echo "Solicitação de amizade enviada com sucesso";
+        return true;
+    }
 }
+catch(PDOException $e){
+    echo "ConexÃ£o falhou: " . $e->getMessage();
+}
+?>
